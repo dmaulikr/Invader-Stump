5a20dbd6c24a771be95cdb6d52268657ce3c0017
Matt <mattlopez.j@gmail.com>
2017-04-24 02:49:54 -0700

    Initial Commit

---
+++ Invader StumpUITests/Invader_StumpUITests.swift
@@ -0,0 +1,36 @@
+//
+//  Invader_StumpUITests.swift
+//  Invader StumpUITests
+//
+//  Created by Lord Lopie on 4/24/17.
+//  Copyright © 2017 Appache Media. All rights reserved.
+//
+
+import XCTest
+
+class Invader_StumpUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
